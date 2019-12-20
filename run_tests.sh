b4067bc2db85492a9ca1edcafc6c12c4bbe9e5a3
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2019-12-20 05:48:29 -0500

    run_tests.sh: Silence ePyDoc/Py2 complaint about Py3 -W value
    (Python 2.7 runtime complains about unrecognized types in the list of
    warnings for Python 3.x to silence)

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "-- MyPy --"
 MYPYPATH="quicktile" mypy --strict-optional --ignore-missing-imports quicktile
 echo "-- Nose (unit tests) --"
 nosetests3 "$@"
 echo "-- ePyDoc (documentation syntax) --"
-epydoc --config setup.cfg quicktile ./*.py
+epydoc --config setup.cfg quicktile ./*.py 2> >(grep -v 'Invalid -W option ignored' >&2)
