416dc10fea1c3e62f6080d06abaadb3fd6eeeedd
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2020-01-17 04:16:04 -0500

    Add the sphinx.ext.githubpages to conf.py
    (Autogenerates the .nojekyll file to make it easier to push to gh-pages)

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
