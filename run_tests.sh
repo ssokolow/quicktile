2b6d12bc3bb31b83f8082a73ecfc65ea1323f857
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2020-01-17 03:17:16 -0500

    Switch run_tests.sh to use Sphinx instead of ePyDoc

--- run_tests.sh
+++ run_tests.sh
@@ -4,5 +4,6 @@ echo "-- MyPy --"
 MYPYPATH="quicktile" mypy quicktile ./*.py functional_harness
 echo "-- Nose (unit tests) --"
 nosetests3 "$@"
-echo "-- ePyDoc (documentation syntax) --"
-epydoc --config setup.cfg quicktile ./*.py functional_harness 2> >(grep -v 'Invalid -W option ignored' >&2)
+echo "-- Sphinx (documentation syntax) --"
+cd docs
+make html
