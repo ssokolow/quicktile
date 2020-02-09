636a062fb4023537093bde88203be99e898577f6
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2020-02-09 03:30:41 -0500

    Initial try at setting up automatic regeneration of gh-pages

---
+++ docs/deploy.sh
@@ -0,0 +1,8 @@
+openssl aes-256-cbc -K $encrypted_0e452a363468_key -iv $encrypted_0e452a363468_iv -in publish-key.enc -out ~/.ssh/publish-key -d
+chmod u=rw,og= ~/.ssh/publish-key
+echo "Host github.com" >> ~/.ssh/config
+echo "  IdentityFile ~/.ssh/publish-key" >> ~/.ssh/config
+git --version
+git remote set-url origin git@github.com:ssokolow/quicktile.git
+git fetch origin -f gh-pages:gh-pages
+ghp-import -n -p -m "Update gh-pages." _build/html
