baaad7bb865d993da8619e231ea27976f494ff28
Stephan Sokolow <ssokolow@users.noreply.github.com>
2025-08-19 16:50:06 -0400

    Allow for varying MyPy versions

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-cd "$(dirname "$(readlink -f "$0")")"
+cd -- "$(dirname -- "$(readlink -f -- "$0")")" || exit 1
 echo "-- MyPy --"
-mypy --exclude=build .
+mypy --exclude=build --warn-unused-ignores .
 echo " -- Ruff (static analysis) --"
 ruff check .
 echo "-- Nose (unit tests) --"
 nosetests3 "$@"
 echo "-- Sphinx (documentation syntax) --"
-cd docs
+cd docs || exit 1
 make coverage | grep -v 'Testing of coverage in the sources finished'
 cat _build/coverage/python.txt
