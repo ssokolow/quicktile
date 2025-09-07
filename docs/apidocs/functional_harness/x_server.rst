7de1b6cb94a78a21038114681ea80f892bc5d366
Stephan Sokolow <ssokolow@users.noreply.github.com>
2025-09-07 07:56:31 -0400

    Switch to pytest-xvfb for functional tests
    (Since I'd been overlooking broken/unimplemented Zaphod-mode support
    anyway, so I might as well just ditch tests which rely on it.)

--- docs/apidocs/functional_harness/x_server.rst
+++
@@ -1,5 +0,0 @@
-X Server Management (``functional_harness/x_server.py``)
-========================================================
-
-.. automodule:: tests.functional_harness.x_server
-   :members:
--- tests/test_functional.py
+++ tests/test_functional.py
@@ -104,37 +104,29 @@ import logging, subprocess, time  # nosec
 import pytest
 from quicktile.wm import WindowManager, Xatom
 
-from tests.functional_harness.env_general import background_proc, os_environ
-from tests.functional_harness.x_server import x_server
+from tests.functional_harness.env_general import background_proc
 
 log = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="module")
 def openbox_session():
-    # TODO: Re-add support for specifying 'Xephyr'
-    with x_server(["Xvfb"], {0: '1024x768x24', 1: '800x600x24'}) as env:
-        # TODO: Use a custom configuration file for Openbox
-        # TODO: Detect once the window manager has started and wait for that
-        #       before running the tests.
-        # TODO: Proper test windows.
-        log.info("Starting test copy of Openbox...")
-        with background_proc(['openbox', '--startup', 'zenity --info'], env):
-            with os_environ(env):
-                wm = WindowManager()
+    log.info("Starting test copy of Openbox...")
+    with background_proc(['openbox', '--startup', 'zenity --info']):
+        wm = WindowManager()
 
-                start = time.time()
-                while wm.get_property(wm.x_root.id, '_NET_SUPPORTING_WM_CHECK',
-                        Xatom.WINDOW) is None:
-                    if time.time() - start > 5:
-                        raise Exception("Timed out waiting for window manager")
-                    else:
-                        time.sleep(0.1)
+        start = time.time()
+        while wm.get_property(wm.x_root.id, '_NET_SUPPORTING_WM_CHECK',
+                Xatom.WINDOW) is None:
+            if time.time() - start > 5:
+                raise Exception("Timed out waiting for window manager")
+            else:
+                time.sleep(0.1)
 
-                yield env
+        yield
 
 
-@pytest.mark.parametrize("command", TEST_SCRIPT)
+@ pytest.mark.parametrize("command", TEST_SCRIPT)
 def test_functional(openbox_session, command):
     """Run the old bank of 'commands don't crash' tests"""
     log.info("Testing command: %s", command)
