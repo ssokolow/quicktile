ec423d2ec0545057cf8786837c440d92def1e916
Stephan Sokolow <ssokolow@users.noreply.github.com>
2025-09-07 00:40:10 -0400

    Fix $PWD bug in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 cd -- "$(dirname -- "$(readlink -f -- "$0")")" || exit 1
-echo "--== Sphinx (documentation syntax) ==--"
-cd docs || exit 1
-make -s SPHINXOPTS=-q coverage | grep -v '+--------------------------------+----------+--------------+'
-echo "--== Sphinx (doctests) ==--"
-make -s doctest
+(
+    echo "--== Sphinx (documentation syntax) ==--"
+    cd docs || exit 1
+    make -s SPHINXOPTS=-q coverage | grep -v '+--------------------------------+----------+--------------+'
+    echo "--== Sphinx (doctests) ==--"
+    make -s doctest
+)
 echo "--== MyPy ==--"
 mypy --exclude=build --warn-unused-ignores .
 echo " --== Ruff (static analysis) ==--"
 ruff check .
 echo "--== PyTest (unit tests) ==--"
-pytest "$@"
+python3 -m pytest "$@"
